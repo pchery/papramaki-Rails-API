1426015a07ce0bf1dd3d16f97a1de80585c96d76
Paul Chery <paulachery@gmail.com>
2016-04-13 20:58:07 -0500

    d

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -1,6 +1,6 @@
 class BalancesController < ApplicationController
   before_action :set_balance, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   # GET /balances
   # GET /balances.json
   def index
