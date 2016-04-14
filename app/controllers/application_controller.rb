58cfab166a89107fd28c64c8d478c288c9a69282
Paul Chery <paulachery@gmail.com>
2016-04-13 20:28:40 -0500

    removed protect from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  #protect_from_forgery with: :null_session
   include DeviseTokenAuth::Concerns::SetUserByToken
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
