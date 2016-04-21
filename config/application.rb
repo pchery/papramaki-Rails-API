a3f9e0f654aabea23769f0131f51ea9f13ff2b06
MarcioPorto <mporto@macalester.edu>
2016-04-21 00:03:36 -0500

    Refactoring

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
 module Papramaki
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -20,6 +19,8 @@ module Papramaki
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.initialize_on_precompile = false
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.api_only = true
