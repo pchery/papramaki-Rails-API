6e97238f90328d6c56dc169a6419de9f69d561f9
MarcioPorto <mporto@macalester.edu>
2016-04-21 00:16:43 -0500

    Fixing Budgets controller

--- app/controllers/api/v1/budgets_controller.rb
+++ app/controllers/api/v1/budgets_controller.rb
@@ -1,19 +1,21 @@
 class Api::V1::BudgetsController < ApplicationController
   before_action :set_budget, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  # respond_to :json
+  respond_to :json
 
   # GET /budgets
   def index
     @budgets = Budget.where(user_id: current_user.id).sort_by(&:created_at).reverse!
-    respond_to do |format|
-      format.json { render }
-    end
+    # respond_to do |format|
+    #   format.json { render }
+    # end
+    respond_with @budgets
   end
 
   # GET /budgets/1
   def show
-    @expenditures = Expenditure.where(budget_id: @budget.id)
+    respond_with @budget
+    # @expenditures = Expenditure.where(budget_id: @budget.id)
   end
 
   # POST /budgets
@@ -22,32 +24,26 @@ class Api::V1::BudgetsController < ApplicationController
     # If nested route:
     # @budget = current_user.budgets.build(budget_params)
 
-    respond_to do |format|
-      if @budget.save
-        format.json { render :show, status: 201, location: [:api, @budget] }
-      else
-        format.json { render json: @budget.errors, status: 442 }
-      end
+    if @budget.save
+      render :show, status: 201, location: [:api, @budget]
+    else
+      render json: @budget.errors, status: 442
     end
   end
 
   # PATCH/PUT /budgets/1
   def update
-    respond_to do |format|
-      if @budget.update(budget_params)
-        format.json { render :show, status: 200, location: [:api, @budget] }
-      else
-        format.json { render json: @budget.errors, status: 442 }
-      end
+    if @budget.update(budget_params)
+      render :show, status: 200, location: [:api, @budget]
+    else
+      render json: @budget.errors, status: 442
     end
   end
 
   # DELETE /budgets/1
   def destroy
     @budget.destroy
-    respond_to do |format|
-      format.json { head 204 }
-    end
+    head 204
   end
 
   private
