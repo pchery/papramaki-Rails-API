b38ee629caa4548b458d019bc493dfb3c0015050
Pradyut <pbansal@macalester.edu>
2016-04-21 14:22:32 -0500

    balances controller updated

---
+++ app/controllers/api/v1/balances_controller.rb
@@ -0,0 +1,57 @@
+class Api::V1::balancesController < ApplicationController
+  before_action :set_balance, only: [:show, :update, :destroy]
+  before_action :authenticate_user!
+  load_and_authorize_resource # CanCanCan helper
+  respond_to :json
+
+  # GET /balances
+  def index
+    @balances = Balance.where(user_id: current_user.id).sort_by(&:created_at).reverse!
+  end
+
+  # GET /balances/1
+  def show
+  end
+
+  # POST /balances
+  def create
+    @balance = Balance.new(balance_params)
+    # If nested route:
+    # @balance = current_user.balances.build(balance_params)
+    @balance.user_id = current_user.id
+    @balance.save
+
+    if @balance.save
+      render :show, status: 201, location: [:api, @balance]
+    else
+      render json: @balance.errors, status: 442
+    end
+  end
+
+  # PATCH/PUT /balances/1
+  def update
+    if @balance.update(balance_params)
+      render :show, status: 200, location: [:api, @balance]
+    else
+      render json: @balance.errors, status: 442
+    end
+  end
+
+  # DELETE /balances/1
+  def destroy
+    @balance.destroy
+    head 204
+  end
+
+  private
+
+  def set_balance
+    @balance = Balance.find(params[:id])
+  end
+
+  def balance_params
+    params.permit(
+      :amount
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       mount_devise_token_auth_for 'User', at: 'auth'
       resources :budgets, only: [:index, :show, :create, :update, :destroy]
+      resources :balances, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
