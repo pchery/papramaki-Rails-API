6da32c30f525aa33e42eee1b0a505ca8514ff2ca
Paul Chery <paulachery@gmail.com>
2016-04-22 14:06:21 -0500

    changed seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 Balance.create(
   amount: 200,
-  user_id: 3
+  user_id: 1
 )
 
 Budget.create(
   amount: 100,
   duration: 12,
-  user_id: 3
+  user_id: 1
 )
 
 Category.create(
   name: 'Food',
   color: '#f32acd',
-  user_id: 3
+  user_id: 1
 )
 
 Expenditure.create(
@@ -21,13 +21,13 @@ Expenditure.create(
       amount: 56,
       budget_id: 1,
       category_id: 1,
-      user_id: 3
+      user_id: 1
     },
     {
       amount: 302,
       budget_id: 1,
       category_id: 1,
-      user_id: 3
+      user_id: 1
     }
   ]
 )
