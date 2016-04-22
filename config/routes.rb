bfe8556d9c1174359217fdccd304c2cd31cc60b8
Paul Chery <paulachery@gmail.com>
2016-04-22 00:55:59 -0500

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,10 @@ Rails.application.routes.draw do
     # The constraints are setting the default API version to 1
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       mount_devise_token_auth_for 'User', at: 'auth'
-      resources :budgets, only: [:index, :show, :create, :update, :destroy]
-      resources :balances, only: [:index, :show, :create, :update, :destroy]
-      resources :categories, only: [:index, :show, :create, :update, :destroy]
-      resources :expenditures, only: [:index, :show, :create, :update, :destroy]
+      resources :budgets, :balances, :categories, :expenditures, only: [:index, :show, :create, :update, :destroy]
+      # resources :balances, only: [:index, :show, :create, :update, :destroy]
+      # resources :categories, only: [:index, :show, :create, :update, :destroy]
+      # resources :expenditures, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
