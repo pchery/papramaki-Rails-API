5b1612d026154e2db31d425ebe26f62be127b00e
MarcioPorto <mporto@macalester.edu>
2016-04-25 13:34:24 -0500

    Back to authenticate_user

--- app/controllers/api/v1/expenditures_controller.rb
+++ app/controllers/api/v1/expenditures_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ExpendituresController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_expenditure, only: [:show, :update, :destroy]
-  before_action :authenticate_api_user!
   load_and_authorize_resource # CanCanCan helper
   respond_to :json
 
