13793fb36221a70381790e26d7734eaa98dce77c
MarcioPorto <mporto@macalester.edu>
2016-04-25 19:10:41 -0500

    Added colors list to helper method

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,6 +1,10 @@
 module CategoriesHelper
   def set_color
-    color_list = ['white', 'blue', 'yellow']
+    color_list = [
+      'red', 'blue', 'green', 'black', 'white', 'gray', 'cyan', 'magenta',
+      'yellow', 'lightgray', 'darkgray', 'grey', 'lightgrey', 'darkgrey',
+      'aqua', 'fuchsia', 'lime', 'maroon', 'navy', 'olive', 'purple', 'silver', 'teal'
+    ]
     number_of_categories = Category.where(user_id: current_user.id).count
     return color_list[number_of_categories]
   end
