fe0e95dfd5b2c249f918a1b93769e3b1502da1d9
MarcioPorto <mporto@macalester.edu>
2016-04-28 17:05:18 -0500

    added more colors to helper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -3,9 +3,16 @@ module CategoriesHelper
     color_list = [
       'red', 'blue', 'green', 'black', 'white', 'gray', 'cyan', 'magenta',
       'yellow', 'lightgray', 'darkgray', 'grey', 'lightgrey', 'darkgrey',
-      'aqua', 'fuchsia', 'lime', 'maroon', 'navy', 'olive', 'purple', 'silver', 'teal'
+      'aqua', 'fuchsia', 'lime', 'maroon', 'navy', 'olive', 'purple', 'silver',
+      'teal', 'red', 'blue', 'green', 'black', 'white', 'gray', 'cyan',
+      'magenta', 'yellow', 'lightgray', 'darkgray', 'grey', 'lightgrey',
+      'darkgrey', 'aqua', 'fuchsia', 'lime', 'maroon', 'navy', 'olive',
+      'purple', 'silver', 'teal', 'red', 'blue', 'green', 'black', 'white',
+      'gray', 'cyan', 'magenta', 'yellow', 'lightgray', 'darkgray', 'grey',
+      'lightgrey', 'darkgrey', 'aqua', 'fuchsia', 'lime', 'maroon', 'navy',
+      'olive', 'purple', 'silver', 'teal'
     ]
     number_of_categories = Category.where(user_id: current_user.id).count
-    return color_list[number_of_categories]
+    color_list[number_of_categories]
   end
 end
