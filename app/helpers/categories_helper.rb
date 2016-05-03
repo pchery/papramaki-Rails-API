521a8d799b2922ed45440795bcdedca1f5fbc231
MarcioPorto <mporto@macalester.edu>
2016-05-02 22:42:33 -0500

    new category colors

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,16 +1,21 @@
 module CategoriesHelper
   def set_color
+    # color_list = [
+    #   'red', 'blue', 'green', 'black', 'white', 'gray', 'cyan', 'magenta',
+    #   'yellow', 'lightgray', 'darkgray', 'grey', 'lightgrey', 'darkgrey',
+    #   'aqua', 'fuchsia', 'lime', 'maroon', 'navy', 'olive', 'purple', 'silver',
+    #   'teal', 'red', 'blue', 'green', 'black', 'white', 'gray', 'cyan',
+    #   'magenta', 'yellow', 'lightgray', 'darkgray', 'grey', 'lightgrey',
+    #   'darkgrey', 'aqua', 'fuchsia', 'lime', 'maroon', 'navy', 'olive',
+    #   'purple', 'silver', 'teal', 'red', 'blue', 'green', 'black', 'white',
+    #   'gray', 'cyan', 'magenta', 'yellow', 'lightgray', 'darkgray', 'grey',
+    #   'lightgrey', 'darkgrey', 'aqua', 'fuchsia', 'lime', 'maroon', 'navy',
+    #   'olive', 'purple', 'silver', 'teal'
+    # ]
     color_list = [
-      'red', 'blue', 'green', 'black', 'white', 'gray', 'cyan', 'magenta',
-      'yellow', 'lightgray', 'darkgray', 'grey', 'lightgrey', 'darkgrey',
-      'aqua', 'fuchsia', 'lime', 'maroon', 'navy', 'olive', 'purple', 'silver',
-      'teal', 'red', 'blue', 'green', 'black', 'white', 'gray', 'cyan',
-      'magenta', 'yellow', 'lightgray', 'darkgray', 'grey', 'lightgrey',
-      'darkgrey', 'aqua', 'fuchsia', 'lime', 'maroon', 'navy', 'olive',
-      'purple', 'silver', 'teal', 'red', 'blue', 'green', 'black', 'white',
-      'gray', 'cyan', 'magenta', 'yellow', 'lightgray', 'darkgray', 'grey',
-      'lightgrey', 'darkgrey', 'aqua', 'fuchsia', 'lime', 'maroon', 'navy',
-      'olive', 'purple', 'silver', 'teal'
+      '#FF0000', '#80FF00', '#00FFFF', '#8000FF',
+      '#FF8000', '#00FF00', '#0080FF', '#FF00FF',
+      '#FFFF00', '#00FF80', '#0000FF', '#FF0080'
     ]
     number_of_categories = Category.where(user_id: current_user.id).count
     color_list[number_of_categories]
