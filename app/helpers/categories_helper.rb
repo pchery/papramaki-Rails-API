e3ace12e548660618524c3a016f1e073b059db48
MarcioPorto <mporto@macalester.edu>
2016-05-04 20:35:31 -0500

    cleaned up CatHelper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,23 +1,5 @@
 module CategoriesHelper
   def set_color
-
-    # color_list = [
-    #   'red', 'blue', 'green', 'black', 'white', 'gray', 'cyan', 'magenta',
-    #   'yellow', 'lightgray', 'darkgray', 'grey', 'lightgrey', 'darkgrey',
-    #   'aqua', 'fuchsia', 'lime', 'maroon', 'navy', 'olive', 'purple', 'silver',
-    #   'teal', 'red', 'blue', 'green', 'black', 'white', 'gray', 'cyan',
-    #   'magenta', 'yellow', 'lightgray', 'darkgray', 'grey', 'lightgrey',
-    #   'darkgrey', 'aqua', 'fuchsia', 'lime', 'maroon', 'navy', 'olive',
-    #   'purple', 'silver', 'teal', 'red', 'blue', 'green', 'black', 'white',
-    #   'gray', 'cyan', 'magenta', 'yellow', 'lightgray', 'darkgray', 'grey',
-    #   'lightgrey', 'darkgrey', 'aqua', 'fuchsia', 'lime', 'maroon', 'navy',
-    #   'olive', 'purple', 'silver', 'teal'
-    # ]
-    # color_list = [
-    #  '#FF0000', '#80FF00', '#00FFFF', '#8000FF',
-    #  '#FF8000', '#00FF00', '#0080FF', '#FF00FF',
-    #  '#FFFF00', '#00FF80', '#0000FF', '#FF0080'
-    # ]
     color_list = ['#16a085', '#2980b9', '#8e44ad', '#f39c12', '#d35400', '#c0392b']
     # colors in color_list = [turquoise, blue, purple, yellow, orange, red]
 
