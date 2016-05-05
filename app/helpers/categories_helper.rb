e4712149292f40215c3c253c862cc9c3211c0b14
kflynnd <kdelaney@macalester.edu>
2016-05-04 23:38:46 -0500

    Added more pie chart colors

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -18,10 +18,11 @@ module CategoriesHelper
     #  '#FF8000', '#00FF00', '#0080FF', '#FF00FF',
     #  '#FFFF00', '#00FF80', '#0000FF', '#FF0080'
     # ]
-    color_list = ['#16a085', '#2980b9', '#8e44ad', '#f39c12', '#d35400', '#c0392b']
-    # colors in color_list = [turquoise, blue, purple, yellow, orange, red]
+    color_list = ['#16a085', '#8e44ad', '#2980b9', '#f39c12', '#d35400', '#c0392b', '#1abc9c', '#9b59b6', '#3498db', '#f1c40f', '#e67e22', '#e74c3c']
+    # colors in color_list = [turquoise, purple, blue, yellow, orange, red, 
+	# 	light turquoise, light purple, light blue, light yellow, light orange, light red]
 
     number_of_categories = Category.where(user_id: current_user.id).count
     color_list[number_of_categories % color_list.count]
   end
-end
+end 
