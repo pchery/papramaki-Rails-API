8126891b23db24556d5ba6752c1fcb3864e12c0c
kflynnd <kdelaney@macalester.edu>
2016-05-04 20:11:08 -0500

    Updated colors in AnalysisFragment

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -12,11 +12,13 @@ module CategoriesHelper
     #   'lightgrey', 'darkgrey', 'aqua', 'fuchsia', 'lime', 'maroon', 'navy',
     #   'olive', 'purple', 'silver', 'teal'
     # ]
-    color_list = [
-      '#FF0000', '#80FF00', '#00FFFF', '#8000FF',
-      '#FF8000', '#00FF00', '#0080FF', '#FF00FF',
-      '#FFFF00', '#00FF80', '#0000FF', '#FF0080'
-    ]
+    # color_list = [
+    #  '#FF0000', '#80FF00', '#00FFFF', '#8000FF',
+    #  '#FF8000', '#00FF00', '#0080FF', '#FF00FF',
+    #  '#FFFF00', '#00FF80', '#0000FF', '#FF0080'
+    # ]
+    color_list = ['#16a085', '#2980b9', '#8e44ad', '#f39c12', '#d35400', '#c0392b']
+    # colors in color_list = [turquoise, blue, purple, yellow, orange, red]
     number_of_categories = Category.where(user_id: current_user.id).count
     color_list[number_of_categories]
   end
